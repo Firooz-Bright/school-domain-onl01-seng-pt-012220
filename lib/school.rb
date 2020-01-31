af3893a03542ea7532fec9eea03f8bf0214c3484
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-01-31 01:53:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School(name)
+  roster={}
+  @name= initilaize=name 
+  
+  def add_student(string, number)
+  roster[:number]<< string
+  end 
+  def grade(arg)
+    return roster[:arg]
+  end 
+   school.sort
+end 
